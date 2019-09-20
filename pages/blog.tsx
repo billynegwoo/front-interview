c3f227708ad5f9564b2b23186c8806cbc98525e7
Kevin Blondel <kevin3.blondel@epitech.eu>
2019-09-20 16:57:16 +0200

    create blog page component

---
+++ pages/blog.tsx
@@ -0,0 +1,14 @@
+import React, {FunctionComponent} from 'react';
+
+import Layout from '../components/Layout';
+
+const Index: FunctionComponent = () => {
+  
+  return (
+    <Layout>
+      <h1 data-testid="page-title">Blog</h1>
+    </Layout>
+  );
+};
+
+export default Index;
