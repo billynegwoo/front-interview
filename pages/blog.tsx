12b6ab8888dff8fe6ee2d869318daa1e7969512a
Kevin Blondel <kevin3.blondel@epitech.eu>
2019-09-20 17:35:28 +0200

    fetch posts

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,12 +1,31 @@
-import React, {FunctionComponent} from 'react';
+import React, {FunctionComponent, useEffect, useState} from 'react';
 
 import Layout from '../components/Layout';
+import fetch from 'isomorphic-unfetch';
+
 
 const Index: FunctionComponent = () => {
-  
+  const [posts, setPosts] = useState([]);
+
+  async function fetchPosts():Promise<void>{
+    const res = await fetch('https://upply-interview.herokuapp.com');
+    const data = await res.json();
+    setPosts(data)
+  }
+
+  useEffect(() => {
+    fetchPosts();
+    return () => window.stop()
+  }, [posts]);
+
   return (
     <Layout>
       <h1 data-testid="page-title">Blog</h1>
+      {
+        posts.map(post => (
+          <div>{post.text}</div>
+        ))
+      }
     </Layout>
   );
 };
